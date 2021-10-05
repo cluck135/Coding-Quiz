1a84081fde73d47a9c67d7a99ee90ebcceda79b9
Casen Luck <casenluck@gmail.com>
2021-10-05 15:39:45 -0400

    timer starts from 60 now

--- Main/Assets/js/script.js
+++ Main/Assets/js/script.js
@@ -1,10 +1,21 @@
 let startButton = document.querySelector(".start-button");
 let timeLeft = document.querySelector(".time-left");
+let question = document.querySelector(".top-text");
+let answers = document.querySelector(".answer-button")
+
 let timer;
-let timerCount = 60;
+let timerCount;
+let answers1 = ["Java","CSS","HTML","Csharp"];
 
 function startGame(){
+    timerCount = 60;
+
+    displayQuestion();
     startTimer();
+}
+
+function displayQuestion(){
+    answers.textContent = answers1;
 }
 
 function endGame(){
@@ -16,8 +27,8 @@ function startTimer(){
         timeLeft.textContent = timerCount;
         if(timerCount == 0){
             clearInterval(timer);
-            endGame()
-        }else if(//have a variable set to all questions being answered then this is executed)
+            endGame();
+        }//else if(//have a variable set to all questions being answered then this is executed)
     }, 1000);
 
 }
